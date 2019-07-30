9e979248cae7842cf36469655a25d61643a39ee0
oFelipeGabriel <felipegabriel@rocketmail.com>
2019-07-30 11:57:38 -0300

    Preparando banco de dados

--- inscricao/admin.py
+++ inscricao/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from inscricao.models import Participante
 
+
+class ParticipanteAdmin(admin.ModelAdmin):
+    pass
 # Register your models here.
+admin.site.register(Participante, ParticipanteAdmin)
--- inscricao/models.py
+++ inscricao/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 class Participante(models.Model):
@@ -6,3 +8,10 @@ class Participante(models.Model):
     email = models.EmailField(max_length=100)
     telefone = models.CharField(max_length=20)
     endereco = models.CharField(max_length=150)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                            on_delete=models.CASCADE, 
+                            null=True, 
+                            blank=True)
+
+    def __str__(self):
+        return self.nome
