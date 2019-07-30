bdc592786ac59785fabd4c731f4d799528d043b9
oFelipeGabriel <felipegabriel@rocketmail.com>
2019-07-30 10:09:47 -0300

    Primeira migration

--- inscricao/models.py
+++ inscricao/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Participante(models.Model):
+    nome = models.CharField(max_length=150)
+    email = models.EmailField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=150)
