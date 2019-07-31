b3c2bc7dfec77d51065f6653c00f5b22d24ff298
oFelipeGabriel <felipegabriel@rocketmail.com>
2019-07-31 09:38:25 -0300

    Criando novas tabelas

--- inscricao/admin.py
+++ inscricao/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from inscricao.models import Participante
+from inscricao.models import Curso
+from inscricao.models import Matricula
 
 
 class ParticipanteAdmin(admin.ModelAdmin):
     pass
+
+class MatriculaeAdmin(admin.ModelAdmin):
+    readonly_fields = ('data',)
 # Register your models here.
 admin.site.register(Participante, ParticipanteAdmin)
+admin.site.register(Curso)
+admin.site.register(Matricula, MatriculaeAdmin)
--- inscricao/models.py
+++ inscricao/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.utils.timezone import now
+
 
 # Create your models here.
 class Participante(models.Model):
@@ -15,3 +17,22 @@ class Participante(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=150)
+    professor = models.CharField(max_length=150)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+    
+    def __str__(self):
+        return self.nome
+
+
+class Matricula(models.Model):
+    data = models.DateField(default=now)
+    aluno = models.ForeignKey(Participante, on_delete=models.SET_NULL,null=True)
+    curso = models.ForeignKey(Curso, on_delete=False)
+
+    def __str__(self):
+        return self.aluno.nome+' / '+self.curso.nome
